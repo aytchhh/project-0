c7290d6a3cdfa5146e6feb1a7a282cd90c977b1a
aytchhh <huajiehehehe@gmail.com>
2024-04-17 09:57:59 +0100

    add endpoint POST /api/articles/:article_id/comments & update test suite

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,10 +1,19 @@
 const { fetchArticleById } = require("../models/articles.model")
-const { fetchCommentsByArticleId } = require("../models/comments.model")
+const { fetchCommentsByArticleId, insertCommentByArticleId } = require("../models/comments.model")
 
 exports.getCommentsByArticleId = (req, res, next) => {
     const {article_id: id} = req.params
     fetchArticleById(id)
     .then(()=>fetchCommentsByArticleId(id))
     .then((comments)=>{res.status(200).send({comments})})
+    .catch(next)
+}
+
+exports.postCommentByArticleId = (req, res, next) => {
+    const {article_id: id} = req.params
+    const newComment = req.body
+    fetchArticleById(id)
+    .then(()=>insertCommentByArticleId(id, newComment))
+    .then((comment)=>{res.status(201).send({comment})})
     .catch(next)
 }
--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,7 @@
 exports.psqlErrorHandler = (err, req, res, next) => {
     if (err.code === '22P02') res.status(400).send({message: 'Bad request'})
+    if (err.code === '23502') res.status(400).send({message: 'Bad request'})
+    if (err.code === '23503') res.status(400).send({message: 'Bad request'})
     next(err)
 };
 
