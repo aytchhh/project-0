9a689eb1f5788d93d0a839cd13e2161d1a5dc4d9
aytchhh <huajiehehehe@gmail.com>
2024-04-18 12:26:21 +0100

    add feature(comment_count) to endpoint GET /api/articles/:article_id & update test suite

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,7 +1,12 @@
 const db = require('../db/connection')
 
 exports.fetchArticleById = (id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+    return db.query(`
+    SELECT articles.*, COUNT(comments.comment_id)::INT AS comment_count 
+    FROM articles LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`, [id])
         .then(({ rows }) => {
             return (rows.length === 0) ? Promise.reject({ status: 404, message: 'article not found' }) : rows[0];
         })
@@ -9,7 +14,7 @@ exports.fetchArticleById = (id) => {
 
 exports.fetchArticles = (topic) => {
     let queryStr = `
-    SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count 
+    SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count 
     FROM articles LEFT JOIN comments
     ON articles.article_id = comments.article_id
     `
--- models/comments.model.js
+++ models/comments.model.js
@@ -14,9 +14,8 @@ exports.insertCommentByArticleId = (id, {username, body}) =>{
 }
 
 exports.removeCommentById = (id) => {
-    return db.query(`SELECT * FROM comments WHERE comment_id = $1;`, [id])
-    .then(({rows})=>{
-        if (rows.length===0) return Promise.reject({status: 404, message: 'comment not found'})
-        return db.query(`DELETE FROM comments WHERE comment_id = $1;`, [id])
+    return db.query(`DELETE FROM comments WHERE comment_id = $1;`, [id])
+    .then((result)=>{
+        if (result.rowCount === 0) return Promise.reject({status: 404, message: 'comment not found'})
     })
 }
